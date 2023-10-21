19fde28523fb6fe8156f8816295c81fd02c10a96
Pablo Gallardo <114663233+pmgallardo@users.noreply.github.com>
2023-10-21 02:05:46 +0200

    Borrar directorio src/popup
    No es necesario emplear un popup por el momento.

--- src/popup/popup.js
+++
@@ -1,5 +0,0 @@
-function modifyText() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {action: "modifyText", replacementText: document.getElementById("replacementText").value});
-  });
-}
